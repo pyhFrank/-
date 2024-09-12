e5f9b3c18a0e2119892f1910e6f5feda39d8ddf2
Frankpyh <yihong_peng@nuaa.edu.cn>
2024-09-11 23:34:22 -0400

    704

---
+++ 数组/搜索插入位置35.java
@@ -0,0 +1,19 @@
+class 搜索插入位置35{
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = left + (right - left) / 2;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
